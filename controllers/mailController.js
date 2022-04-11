0abed42af5e5bc88635a2324233faf1f4079ed6f
GiftBanda <bandagift42@gmail.com>
2022-04-11 16:34:40 +0200

    still testing

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -2,10 +2,10 @@ const sgMail = require('@sendgrid/mail');
 
 //send email
 exports.sendEmail = (req, res) => {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY || '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');
 
     const msg = {
-        to: 'gift@bongohive.co.zm', // Change to your recipient
+        to: req.body.email, // Change to your recipient
         from: 'bandagift42@gmail.com', // Change to your verified sender
         subject: 'Sending with SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
