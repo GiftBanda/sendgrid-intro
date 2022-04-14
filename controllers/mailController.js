f3e410cfe38ac1e783977a01720a0fdfee2930e2
GiftBanda <bandagift42@gmail.com>
2022-04-14 17:56:24 +0200

    send fix

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -6,7 +6,7 @@ exports.sendEmail = (req, res) => {
 
     const msg = {
         to: req.body.email, // Change to your recipient
-        from: 'bandagift42@gmail.com', // Change to your verified sender
+        from: 'test@email.com', // Change to your verified sender
         subject: 'Sending with SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
